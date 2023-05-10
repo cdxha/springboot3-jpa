dc0f300a50f3a228d0e9ef00259106faa7fa08f2
cdxha <henriqueaguiar22@hotmail.com>
2023-05-10 16:15:02 -0300

    H2 database, test profile, JPA

--- src/main/java/com/carlos/course/CourseApplication.java
+++ src/main/java/com/carlos/course/CourseApplication.java
@@ -8,6 +8,17 @@ public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		/**
+		 * spring boot: Framework para criar a aplicação.
+		 * apachetomcat: Container web para executar a aplicação.
+		 * maven: Gerenciador de dependencias.
+		 * h2: Banco de dados em memória para testes.
+		 * postman: Ferramenta para testar as requisições.
+		 * Produção na nuvem - portfólio:
+		 * PostgreSQL
+		 * Heroku
+		 */
+		
 		
 	}
 
